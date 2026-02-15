7b147686acabc7ad7663a75d252439a5137a6818
Harshika Sharma <harshikas621@gmail.com>
2026-02-15 18:23:39 +0530

    Initial backend commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "../config/db.js";
+import authRoutes from "../routes/authRoutes.js";
+import userRoutes from "../routes/userRoutes.js";
+import { errorHandler } from "../middleware/errorMiddleware.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use(errorHandler);
+
+export default app;
